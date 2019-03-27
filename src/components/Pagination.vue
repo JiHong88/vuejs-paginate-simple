c615b9b157e19a67a6dcfeb54f81159c8d1b2bab
jihong.lee <0125ses@hanmail.net>
2019-03-28 00:30:58 +0900

    add v-model value

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,15 +1,15 @@
 <template>
     <div class="vuejs-pagination-component">
         <ul class="pagination">
-            <li class="page-item" :class="{'disabled': currentPage === 1}"><a tabindex="-1" @click="pickPage(1)" class="page-link"><span>«</span></a></li>
-            <li class="page-item" :class="{'disabled': currentPage === 1}"><a tabindex="-1" @click="movePage(-1)" class="page-link"><span>‹</span></a></li>
+            <li class="page-item navi" :class="{'disabled': currentPage === 1}"><a tabindex="-1" @click="pickPage(1)" class="page-link"><span>«</span></a></li>
+            <li class="page-item navi" :class="{'disabled': currentPage === 1}"><a tabindex="-1" @click="movePage(-1)" class="page-link"><span>‹</span></a></li>
 
             <li v-for="(page, index) in pages" :key="index" class="page-item" :class="{'active': currentPage === page}">
                 <a @click="pickPage(page)" tabindex="-1" class="page-link">{{ page }}</a>
             </li>
 
-            <li class="page-item" :class="{'disabled': currentPage >= totalPages}"><a @click="movePage(1)" tabindex="-1" class="page-link"><span>›</span></a></li>
-            <li class="page-item" :class="{'disabled': currentPage >= totalPages}"><a @click="pickPage(totalPages)" tabindex="-1" class="page-link"><span>»</span></a></li>
+            <li class="page-item navi" :class="{'disabled': currentPage >= totalPages}"><a @click="movePage(1)" tabindex="-1" class="page-link"><span>›</span></a></li>
+            <li class="page-item navi" :class="{'disabled': currentPage >= totalPages}"><a @click="pickPage(totalPages)" tabindex="-1" class="page-link"><span>»</span></a></li>
         </ul>
     </div>
 </template>
@@ -37,17 +37,20 @@ export default {
         }
     },
     computed: {
+        pageCountLimit() {
+            return this.pageCount
+        },
         totalPages() {
             return Math.ceil(this.totalResult / this.rowsPerPage)
         },
         padding() {
-            return Math.floor(this.pageCount / 2)
+            return Math.floor(this.pageCountLimit / 2)
         },
         pages() {
             const pages = []
             const index = this.currentPage - this.padding;
-            const start = index < 1 ? 1 : index + this.pageCount > this.totalPages ? this.totalPages - this.pageCount + 1 : index
-            const end = start + (this.pageCount > this.totalPages ? this.totalPages || 1 : this.pageCount)
+            const start = index < 1 ? 1 : index + this.pageCountLimit > this.totalPages ? this.totalPages - this.pageCountLimit + 1 : index
+            const end = start + (this.pageCountLimit > this.totalPages ? this.totalPages || 1 : this.pageCountLimit)
             
             for (let i = start; i < end; i++) {
                 pages.push(i)
@@ -71,7 +74,7 @@ export default {
             this.currentPage = pageNum
         },
         movePage(moveNum) {
-            this.currentPage = ((Math.ceil(this.currentPage / this.pageCount) + moveNum) * this.pageCount) - this.pageCount + 1
+            this.currentPage = ((Math.ceil(this.currentPage / this.pageCountLimit) + moveNum) * this.pageCountLimit) - this.pageCountLimit + 1
         }
     }
 }
